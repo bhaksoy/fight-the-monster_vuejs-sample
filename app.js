9084d23019cf90d23cd2d936625597297e7c0eef
Bengisu Hayat Aksoy <145993791+bhaksoy@users.noreply.github.com>
2024-01-02 13:55:45 +0000

    button functions adjusted

--- app.js
+++ app.js
@@ -13,18 +13,32 @@ new Vue({
 
         },
         attack: function(){
+            var point = Math.ceil(Math.random() * 10);
+            this.monster_heal -= point;
+            this.monster_attack();
 
         },
         special_attack: function(){
+            var point = Math.ceil(Math.random() * 25);
+            this.monster_heal -= point;
+            this.monster_attack();
 
         },
         heal_up: function(){
+            var point = Math.ceil(Math.random() * 20);
+            this.player_heal += point;
+            this.monster_attack();
 
         },
         give_up: function(){
+            this.player_heal = 0; 
 
         },
-
+        monster_attack: function(){
+            var point = Math.ceil(Math.random() * 15);
+            this.player_heal -= point;
+        }
+   
 
     }
 })
